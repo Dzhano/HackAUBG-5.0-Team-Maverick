523128f71ef1cd8d62174748d9f1e676dc129d21
SlazyPlayz <56247845+SlazyPlayz@users.noreply.github.com>
2023-04-01 18:02:15 +0300

    Add files via upload

---
+++ databaseInC#/UnityGame/UnityGame/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnityGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user1 = new User("name1", 4, 13);
+            User user2 = new User("name2", 5, 25);
+            User user3 = new User("name3", 2, 34);
+            User user4 = new User("user4", 12, 43);
+
+            UserRepository rep = new UserRepository();
+            rep.AddUser(user1);
+            rep.AddUser(user2);
+            rep.AddUser(user3);
+            rep.AddUser(user4);
+
+            foreach (var user in rep.GetAllUsers())
+            {
+                Console.WriteLine(user.Name + " " + user.NumGames);
+            }
+        }
+    }
+}
