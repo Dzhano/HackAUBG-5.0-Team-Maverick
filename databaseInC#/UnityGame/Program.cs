519af6b1a4fd784469143644d351058be96de715
SlazyPlayz <56247845+SlazyPlayz@users.noreply.github.com>
2023-04-02 12:27:39 +0300

    Add files via upload

---
+++ databaseInC#/UnityGame/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnityGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Registering users: ");
+            int num = int.Parse(Console.ReadLine());
+
+            UserRepository rep = new UserRepository();
+
+            for (int i = 0; i < num; i++)
+            {
+                Console.Write("Username: ");
+                string username = Console.ReadLine();
+                User user = new User(username, 0, 0);
+                rep.AddUser(user);
+            }
+
+            /*User user1 = new User("name1", 4, 13);
+            User user2 = new User("name2", 5, 25);
+            User user3 = new User("name3", 2, 34);
+            User user4 = new User("user4", 12, 43);
+
+            rep.AddUser(user1);
+            rep.AddUser(user2);
+            rep.AddUser(user3);
+            rep.AddUser(user4);*/
+
+            foreach (var user in rep.GetAllUsers())
+            {
+                Console.WriteLine(user.Name + " " + user.NumGames);
+            }
+        }
+    }
+}
---
+++ databaseInC#/UnityGame/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace UnityGame
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public int NumGames { get; set; }
+        public int TotalPoints { get; set; }
+        public string Name { get; set; }
+
+        public User() { }
+
+        public User(string name, int numGames, int totalPoints)
+        {
+            Name = name;
+            NumGames = numGames;
+            TotalPoints = totalPoints;
+        }
+
+        public User(int id, int numGames, int totalPoints, string name)
+        {
+            Id = id;
+            NumGames = numGames;
+            TotalPoints = totalPoints;
+            Name = name;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(Name + " " + TotalPoints + " " + NumGames + " " + Id);
+        }
+    }
+}
